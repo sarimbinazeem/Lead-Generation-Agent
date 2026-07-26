f8d08ab575a723e77be50dc0076b9b5e3be7de23
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-26 14:32:52 +0500

    Phase 2: Parsing

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 Purpose -> starts the file and co-ordinates everything
 """
 
+from parser import parse_prompt
+
+
 def main():
     print("=" * 50)
     print("Lead Generation Agent Started")
     print("=" * 50)
+
+    prompt = input("Enter Your Search Query: ")
+    try:
+        business_type, location = parse_prompt(prompt)
+
+        print("\nExtraction Successful!")
+        print(f"Business Type : {business_type}")
+        print(f"Location      : {location}")      
+    
+    except Exception as e:
+        print("\nAn error occurred while parsing the prompt.")
+        print(e)      
 
 
 if __name__ == "__main__":
--- parser.py
+++ parser.py
@@ -1,4 +1,61 @@
 """
 Purpose -> Extracts the business type and locations from user prompt
 
+OpenAI will extract the info
 """
+
+import os
+import sys
+import json #to handle json response
+
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+sys.stdout.reconfigure(encoding = "utf-8")
+
+client = OpenAI(
+    api_key=os.getenv("DO_API_KEY"),
+    base_url=os.getenv("DO_BASE_URL"),
+)
+
+def parse_prompt(prompt):
+    """
+    It takes user prompt 
+
+    this function extracts the business type and lcoations from the prompt
+    
+    """
+
+    response = client.chat.completions.create(
+        model=os.getenv("MODEL"),
+        temperature=0,
+        response_format={"type": "json_object"},        
+        messages=[
+            {
+                "role":"system",
+                "content":(
+                    "You are an AI Agent that extracts lead information only\n"
+                    "Return only the JSON Foramt response in the following format: \n"
+                    '{'
+                    '"business_type":"...",'
+                    '"location":"..."'
+                    '}'
+                    
+                ),
+
+            },
+            {
+                "role":"user",
+                "content":prompt,
+            },
+        ]
+
+    )
+
+    result = json.loads(response.choices[0].message.content)
+
+    return (
+        result.get("business_type",""),
+        result.get("location",""),
+    )
